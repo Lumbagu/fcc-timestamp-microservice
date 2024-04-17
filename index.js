531231b9a065ebc8769ec4e46201c32612b09428
Lumbagu <masiver.ae@yahoo.com>
2024-04-17 21:40:49 +0200

    my solution

--- index.js
+++ index.js
@@ -10,8 +10,35 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
 });
 
-app.get("/api/hello", (req, res) => {
-    res.json({ greeting: "hello API" });
+app.get("/api/:date?", (req, res, next) => {
+    if (!req.params.date) {
+        req.date = new Date();
+        return next();
+    }
+
+    if (!isNaN(req.params.date)) {
+        req.date = new Date(Number(req.params.date));
+        return next();
+    }
+
+    const date = new Date(req.params.date);
+    if (!isNaN(date)) {
+        req.date = date;
+        return next();
+    }
+
+    req.error = "Invalid Date";
+    next();
+}, (req, res) => {
+    if (req.error) {
+        res.json({ error: req.error });
+        return;
+    }
+
+    res.json({
+        unix: req.date.valueOf(),
+        utc: req.date.toUTCString()
+    });
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
